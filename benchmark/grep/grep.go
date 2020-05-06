96146f70b479265d43deadb092d0bd0c83648d33
derailed <fernand.galiana@gmail.com>
2020-05-06 12:15:12 -0600

    updates

--- benchmark/grep/grep.go
+++
@@ -1,8 +0,0 @@
-// © 2020 Imhotep Software LLC. All rights reserved.
-
-package grep
-
-// Count returns the number of occurrence of a word in a line.
-func Count(word, line string) int {
-	// !!YOUR_CODE!!
-}
