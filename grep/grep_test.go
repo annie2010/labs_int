ab52c5df79043211f5522acc1948c6e067368af7
derailed <fernand.galiana@gmail.com>
2020-05-06 11:52:35 -0600

    updates

---
+++ grep/grep_test.go
@@ -0,0 +1,28 @@
+// Copyright 2020 Imhotep Software
+// All material is licensed under the Apache License Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package grep_test
+
+import (
+	"testing"
+
+	"github.com/gopherland/labs_int/grep"
+	"gotest.tools/assert"
+)
+
+func TestWordCountV1(t *testing.T) {
+	<<!!YOUR_CODE!!>> -- Use a table test with subtests and make sure your implementation is valid
+}
+
+func TestWordCountV2(t *testing.T) {
+	<<!!YOUR_CODE!!>> -- Use a table test with subtests and make sure your implementation is valid
+}
+
+func BenchmarkWordCountV1(b *testing.B) {
+	<<!!YOUR_CODE!!>> -- Implement the v1 benchmark
+}
+
+func BenchmarkWordCountV2(b *testing.B) {
+	<<!!YOUR_CODE!!>> -- Implement the v2 benchmark
+}
