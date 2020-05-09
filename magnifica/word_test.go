868b8065710a7a3d213743ae9ca591c6abf07a02
gopherland <fernand@gopherland.io>
2020-05-09 12:44:08 -0600

    updates

---
+++ magnifica/word_test.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Imhotep Software
+// All material is licensed under the Apache License Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package magnifica_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/gopherland/labs_int/magnifica"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEntryMarshal(t *testing.T) {
+	<<!!YOUR_CODE!!>> -- marshall a magnifica dictionary entry to ACME
+}
+
+func TestEntryUnmarshal(t *testing.T) {
+	<<!!YOUR_CODE!!>> -- unmarshal an ACME entry to a Magnifica dictionary entry
+}
+
+const raw = `{"dictionary_location":"/acme/english/slang","dictionary_word":"bumblebeetuna","political_correctness":false}`
